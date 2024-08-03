19ef9352f4cc69f586a6c0f7113fce6f93220536
Mohamed Taha Hassen <mt337848@gmail.com>
2024-08-03 18:55:22 +0300

    search done

--- lib/Features/Search/presentation/views/widget/search_view_body.dart
+++ lib/Features/Search/presentation/views/widget/search_view_body.dart
@@ -1,6 +1,9 @@
 import 'package:bookly_app/Features/Search/presentation/views/widget/search_view_text_Field.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../../core/styles/text_styel.dart';
+import '../../../../Home/presentation/Views/Widget/best_sellar_list_view_item.dart';
+
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
@@ -9,10 +12,35 @@ class SearchViewBody extends StatelessWidget {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 30),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           SearchViewTextField(),
+          SizedBox(height: 10,),
+          Text(
+            "Search ",
+            style: AppFontStyles.textStyle20,
+          ),
+          SizedBox(height: 15,),
+
+          Expanded(child: SearchResultVerticalListView())
         ],
       ),
     );
   }
 }
+class SearchResultVerticalListView extends StatelessWidget {
+  const SearchResultVerticalListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      physics: const BouncingScrollPhysics(),
+      itemBuilder: (context, index) => const BestSellerListViewItem(),
+      separatorBuilder: (context, index) => const SizedBox(
+        height: 20,
+      ),
+      itemCount: 15,
+    );
+  }
+}
+
