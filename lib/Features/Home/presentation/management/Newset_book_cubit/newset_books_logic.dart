b7ca2e2b0b9c003bbc358465f558c5e48e0adbff
Mohamed Taha Hassen <mt337848@gmail.com>
2024-08-05 15:57:50 +0300

    Newest Books cubit  Done

---
+++ lib/Features/Home/presentation/management/Newset_book_cubit/newset_books_logic.dart
@@ -0,0 +1,22 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly_app/Features/Home/presentation/management/Newset_book_cubit/newset_book_state.dart';
+
+import '../../../data/repos/home_repo.dart';
+
+class NewestBooksCubit extends Cubit<NewestBookState> {
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+      (failure) {
+        emit(NewestBooksFailure(failure.errorMessage));
+      },
+      (books) {
+        emit(NewestBooksSuccess(books));
+      },
+    );
+  }
+}
