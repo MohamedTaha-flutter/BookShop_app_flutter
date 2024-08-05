7c7fc38b9edef4212eb5e04c401622c2ebcde376
Mohamed Taha Hassen <mt337848@gmail.com>
2024-08-05 15:47:55 +0300

    cubit  logic Featured Books

--- lib/Features/Home/management/featured_books_cubit/featured_books_logic.dart
+++ lib/Features/Home/management/featured_books_cubit/featured_books_logic.dart
@@ -1,7 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly_app/Features/Home/data/repos/home_repo.dart';
 import 'package:bookly_app/Features/Home/management/featured_books_cubit/featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+  final HomeRepo homeRepo;
 
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+      (failure) {
+        emit(FeaturedBooksFailure(failure.errorMessage));
+      },
+      (books) {
+        emit(FeaturedBooksSuccess(books));
+      },
+    );
+  }
 }
