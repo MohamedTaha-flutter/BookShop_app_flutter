de43be457937f93f8d24be88df23c507e0c532f9
Mohamed Taha Hassen <mt337848@gmail.com>
2024-08-05 17:16:28 +0300

    create bloc provider and get it locator

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:bookly_app/Features/Home/data/repos/home_repo_impl.dart';
+import 'package:bookly_app/Features/Home/presentation/management/Newset_book_cubit/newset_books_logic.dart';
+import 'package:bookly_app/Features/Home/presentation/management/featured_books_cubit/featured_books_logic.dart';
 import 'package:bookly_app/core/app_router.dart';
+import 'package:bookly_app/core/service/service_locator.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'core/styles/color.dart';
 
 void main() {
-  runApp( const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -14,16 +19,23 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      theme: ThemeData.dark().copyWith(
-          scaffoldBackgroundColor: ColorApp.mainColor,
-          appBarTheme: const AppBarTheme(
-              systemOverlayStyle: SystemUiOverlayStyle(
-                  statusBarIconBrightness: Brightness.light,
-                statusBarColor: ColorApp.mainColor
-              ))),
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+            create: (context) => FeaturedBooksCubit(getIt.get<HomeRepoImpl>())),
+        BlocProvider(
+            create: (context) => NewestBooksCubit(getIt.get<HomeRepoImpl>())),
+      ],
+      child: MaterialApp.router(
+        theme: ThemeData.dark().copyWith(
+            scaffoldBackgroundColor: ColorApp.mainColor,
+            appBarTheme: const AppBarTheme(
+                systemOverlayStyle: SystemUiOverlayStyle(
+                    statusBarIconBrightness: Brightness.light,
+                    statusBarColor: ColorApp.mainColor))),
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
